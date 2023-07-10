7f898e7fc1e8dbb01ce60d9c8a6193745049b14d
CokeLee777 <akak640@kyonggi.ac.kr>
2023-07-10 21:21:45 +0900

    feat: add profile image and remove prev image

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -33,7 +31,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} width={217} height={237} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
